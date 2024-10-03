5cf1598976dc0afefbcb8a3fbf6c64e7c883f014
Avinash Kumar Gupta <robozealous@gmail.com>
2024-10-04 01:41:20 +0530

    Related products feature added to the product page

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const Item = ({id, name, image, new_price, old_price}) => {
   return (
     <div className={styles.item}>
-        <Link to={`/product/${id}`}><img src={image} alt="item-iamge"/></Link>
+        <Link to={`/product/${id}`}><img onClick={window.scrollTo(0,0)} src={image} alt="item-iamge"/></Link>
         <p>{name}</p>
         <div className={styles.itemPrice}>
             <div className={styles.itemPriceNew}>
