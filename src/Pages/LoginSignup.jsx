2ae251a1115f7ce14e9e66ee1e43f4f15a2894b5
Avinash Kumar Gupta <robozealous@gmail.com>
2024-10-05 01:33:52 +0530

    login signup page setup and modified to provide both functinality by the click of button which switches from login and signup and vice versa

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,21 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './CSS/LoginSignup.module.css'
 
 const LoginSignup = () => {
+
+  const [state, setState] = useState("Login");
+  const [formData, setFormData] = useState({
+    username:"",
+    password:"",
+    email:"",
+  })
+
+  const changeHandler = (e) => {
+    setFormData({...formData, [e.target.name]:e.target.value})
+  }
+
+  const login = async () => {
+    console.log("login function executed", formData)
+  }
+
+  const signup = async () => {
+    console.log("signup function executed", formData) 
+    
+  }
+
+
   return (
     <div className={styles.loginSingup}>
       <div className={styles.loginSignupContainer}>
-        <h1>Sign Up</h1>
+        <h1>{state}</h1>
         <div className={styles.loginSignUpFields}>
-          <input type="text" placeholder='Your Name' />
-          <input type="email" placeholder='Email Address' />
-          <input type="password" placeholder='Password' />
-          <button>Continue</button>
+          {state === "Sign Up" ? <input type="text" name='username' value={formData.username} onChange={changeHandler} placeholder='Your Name' /> : <></>}
+          <input type="email" name='email' value={formData.email} onChange={changeHandler} placeholder='Email Address' />
+          <input type="password" name='password' value={formData.password} onChange={changeHandler} placeholder='Password' />
+          <button onClick={()=>{state==="Login"?login():signup()}}>Continue</button>
         </div>
-        
-        <p className={styles.loginSignupLogin}>Already have an account? <span>Login here</span></p>
+        {state === "Sign Up" ?
+          <p className={styles.loginSignupLogin}>Already have an account? <span onClick={()=>{setState("Login")}}>Login here</span></p> :
+          <p className={styles.loginSignupLogin}>Create an account? <span onClick={()=>{setState("Sign Up")}}>Click here</span></p>
+        }
         <div className={styles.loginSignupAgree}>
-          <input type="checkbox" name='' id=''/>
+          <input type="checkbox" name='' id='' />
           <p>By continuing, I agree to the terms of use & privacy policy</p>
         </div>
       </div>
