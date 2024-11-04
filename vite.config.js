4399114c2a932692073b370906ecee936761072f
Avinash Kumar Gupta <robozealous@gmail.com>
2024-11-04 22:47:15 +0530

    last changes made

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/react_tooltip/', // replace <REPOSITORY_NAME> with your actual repo name
+});
